eb1816fe9ff12fd5863e1d06fdc77e5ae06d8287
Charan <charansamsani.official@gmail.com>
2024-10-23 00:01:57 +0530

    Create footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+function footer() {
+    
+    return (
+        <div className="footer">
+            <p>© 2024 Charan Samsani. All rights reserved.</p>
+            <p>charansamsani.official@gmail.com</p>
+        </div>
+    );
+}
+
+export default footer;
